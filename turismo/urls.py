526467f048209da42c34bf186635b667f937fdc9
mauriciosantana <mauriciomdsan@gmail.com>
2020-07-04 20:06:12 -0300

    textbox provisorio

--- turismo/urls.py
+++ turismo/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import TurismoListView
+from .views import TurismoListView, busca
+from . import views
 
 
 urlpatterns = [
-    path('', TurismoListView.as_view(), name='home')
+    path('', TurismoListView.as_view(), name='home'),
 
+    path('busca', views.busca, name='busca'),
     #path('', views.home, name='home'),
 ]
--- turismo/views.py
+++ turismo/views.py
@@ -8,13 +8,11 @@ class TurismoListView(ListView):
     model = Turismo
     template_name = 'turismo/home.html'
 
-def login(request):
+def busca(request):
 	if request.method == 'POST':
-		#user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
 		if user is not None:
-			#auth.login(request, user)
 			return redirect('home')
 		else:
-			 return render(request, 'accounts/login.html', {'error': 'username or password is incorrect.'})
+			 return render(request, 'turismo/busca.html', {'error': 'username or password is incorrect.'})
 	else:
-		return render(request, 'accounts/login.html')
+		return render(request, 'turismo/busca.html')
